2f530d443be6b040fccd72ed566016dfb8bc6903
Nikita Sherbakov <nikita@sherbakov.dev>
2021-10-22 14:36:17 +0300

    revert user agent

--- src/api.ts
+++ src/api.ts
@@ -367,9 +367,6 @@ export class OpenSeaAPI {
       headers: {
         ...(apiKey ? { "X-API-KEY": apiKey } : {}),
         ...(opts.headers || {}),
-
-        "User-Agent":
-          "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Safari/537.36",
       },
     };
 
